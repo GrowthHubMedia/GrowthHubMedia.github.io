e7dd13efc443c9db78d7a689dd11d1b48f19041f
Cihat Yildiz <cihat.yildiz@gmail.com>
2025-08-23 22:18:31 -0700

    adding new updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from 'url';
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig(({ mode }) => ({
+  base: '/pacificadvisors.github.io/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
