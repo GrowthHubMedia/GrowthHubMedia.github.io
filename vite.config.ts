895fc3d465f1bcad442a478ed837fb237a50b3e7
Cihat Yildiz <cihat.yildiz@gmail.com>
2025-08-24 18:41:31 -0700

    enable dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
-  base: '/GrowthHubMedia.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
